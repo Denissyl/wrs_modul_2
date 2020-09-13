14f2b9fc9104e9403db4105beef013797be62137
Denis <Denis-syl@yandex.ru>
2020-09-13 12:18:34 +0500

    save

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
 <?php
+// Заголовки на ответ.
+header("Content-Type: application/json; charset=utf-8");
+
+
+
+
+
+
+
+
+if (isset($_GET['api'])) {
+
+    if (isset($_GET['signup'])) {
+
+        $array = array('id' => 1);
+
+        header('HTTP/1.0 201 Created');
+        api_response($array);
+    }
+    else {
+        header('HTTP/1.0 404 Created');
+        exit;
+    }
+}
+
+else {
+
+    header('HTTP/1.0 404 Created');
+    exit;
+}
+
+
+function api_response($array) {
+    header("Content-Type: application/json; charset=utf-8");
+    echo json_encode($array);
+    exit;
+}
